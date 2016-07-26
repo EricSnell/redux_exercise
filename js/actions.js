4b06358a1c81083713b66b82d2396aaf22868449
Sean Jun <seanjun21@gmail.com>
2016-07-26 12:02:13 -0700

    Finished linking Redux-React

--- js/actions.js
+++ js/actions.js
@@ -1,18 +1,18 @@
 var ADD_REPOSITORY = 'ADD_REPOSITORY';
-var addRepository = function(repository) {
-    return {
-        type: ADD_REPOSITORY,
-        repository: repository
-    }
+var addRepository = function( repository ) {
+  return {
+    type: ADD_REPOSITORY,
+    repository: repository
+  };
 };
 
 var RATE_REPOSITORY = 'RATE_REPOSITORY';
-var rateRepository = function(repository, rating) {
-    return {
-        type: RATE_REPOSITORY,
-        repository: repository,
-        rating: rating
-    };
+var rateRepository = function( repository, rating ) {
+  return {
+    type: RATE_REPOSITORY,
+    repository: repository,
+    rating: rating
+  };
 };
 
 exports.ADD_REPOSITORY = ADD_REPOSITORY;
